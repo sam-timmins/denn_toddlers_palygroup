6f755d407ccbac4c370c7daecf7decf2c4de0b8d
Sam Timmins <sam.timmins1@gmail.com>
2022-07-13 20:49:47 +0100

    Add contact form

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.contrib import messages
 
-# Create your views here.
+
+def contact(request):
+    """ A view to return the contact page """
+
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message_message = request.POST['message']
+
+        name = message_name.strip()
+        message = message_message.strip()
+
+        if not message:
+            saved_name = name
+            saved_email = message_email
+
+            messages.error(
+                request,
+                'Please include a message so we can help.'
+                )
+            return render(request, 'contact/contact.html', {
+                'saved_name': saved_name,
+                'saved_email': saved_email,
+            })
+
+        elif not name:
+            saved_email = message_email
+            saved_message = message
+
+            messages.error(
+                request,
+                'Please include your name.'
+                )
+            return render(request, 'contact/contact.html', {
+                'saved_message': saved_message,
+                'saved_email': saved_email,
+            })
+
+        else:
+            send_mail(
+                f'Message from {name}',
+                message_message,
+                message_email,
+                ['sam.timmins1@gmail.com', ],
+                )
+
+        return render(request, 'contact/contact.html', {
+            'name': name,
+        })
+
+    else:
+        return render(request, 'contact/contact.html')
--- denn_toddlers/settings.py
+++ denn_toddlers/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOP')
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['denn-toddlers.herokuapp.com', '127.0.0.1']
 
